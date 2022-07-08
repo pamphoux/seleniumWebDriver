343aca722b0265fb22cff9d258c9198821b010bc
pamphoux <108572694+pamphoux@users.noreply.github.com>
2022-07-08 22:35:43 +0200

    Create PositiveTestsStep1.java

---
+++ PositiveTestsStep1.java
@@ -0,0 +1,19 @@
+package net.atlassian.pamphoux;
+
+public class PositiveTests {
+	
+	public void loginTest() {
+//		Create driver
+//		
+//		Open test page
+//		enter username
+//		enter password
+//		click login button
+//		
+//		Verifications
+//		new url
+//		logout button is visible
+//		succesful login message
+	}
+
+}
